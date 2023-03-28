4ac0a418e090a5d62a0c7a54ef727e29998734fa
Ruben Matias Tiscar <rmatiast@emeal.nttdata.com>
2023-03-28 11:09:48 +0200

    Initial commit

---
+++ src/main/java/com/example/eurekaserverregistry/EurekaServerRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekaserverregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerRegistryApplication.class, args);
+	}
+
+}
