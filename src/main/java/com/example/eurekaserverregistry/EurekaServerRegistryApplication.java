22ce569adbfebcdbeadaad3d80f189d5ae1df6ad
Ruben Matias Tiscar <rmatiast@emeal.nttdata.com>
2023-03-30 14:20:05 +0200

    all good

--- src/main/java/com/example/eurekaserverregistry/EurekaServerRegistryApplication.java
+++ src/main/java/com/example/eurekaserverregistry/EurekaServerRegistryApplication.java
@@ -2,12 +2,14 @@ package com.example.eurekaserverregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerRegistryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerRegistryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerRegistryApplication.class, args);
+    }
 
 }
